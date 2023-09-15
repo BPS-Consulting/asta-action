859a194cf7a877b42e07b416d426214e7c6735da
Don Isaac <donald.isaac@gmail.com>
2023-09-15 13:16:59 -0400

    build: switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externalsPresets: {
+    node: true
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }
+    ]
+  },
+  resolve:{
+    extensions: ['.ts', '.js', '.json']
+  } 
+}
